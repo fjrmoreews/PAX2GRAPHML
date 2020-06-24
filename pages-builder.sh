430a229fb8a6851bbb97b45fdb5436c6d92ebcbc
François Moreews <fjrmoreews@gmail.com>
2020-06-24 16:44:10 +0200

    doc builder from sphinx-2-gh-pages-converter.sh

--- pages-builder.sh
+++ pages-builder.sh
@@ -12,6 +12,7 @@ git clean -df
 git checkout -- .
 git fetch --all
 
+echo "step 1"
 # build html docs from sphinx files
 #sphinx-build -b html . "$buildDirectory"
 
@@ -21,27 +22,37 @@ cp -r  $SOURCEDIR/* ./
 branch_name=gh-pages
 if [ $(git branch --list "$branch_name") ]
 then
+
+        echo "step 2"
+
 	git stash
 	git checkout $branch_name
 	git pull origin $branch_name
 	#git stash apply
 	git checkout stash -- . # force git stash to overwrite added files
 else
+        echo "step 3"
+
 	git checkout --orphan "$branch_name"
 fi
 
 if [ -d "$buildDirectory" ]
 then
+
+        echo "step 4"
 	ls | grep -v _build | xargs rm -r
 	mv _build/* . && rm -rf _build
 	git add .
+        
 	git commit -m "new pages version $(date)"
 	git push origin gh-pages
+        echo "step 5"
+
 	# github.com recognizes gh-pages branch and create pages
 	# url scheme https//:[github-handle].github.io/[repository]
 else
 	echo "directory $buildDirectory does not exists"
 fi
-
+echo "step 6"
 git checkout master
 
