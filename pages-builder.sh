b1cb2dba9c40506349f7113b23179322aef112e8
François Moreews <fjrmoreews@gmail.com>
2020-06-24 16:49:30 +0200

    doc builder from sphinx-2-gh-pages-converter.sh

--- pages-builder.sh
+++ pages-builder.sh
@@ -27,7 +27,7 @@ then
 
 	git stash
 	git checkout $branch_name
-	git pull origin $branch_name
+	git pull origin $branch_name --allow-unrelated-histories
 	#git stash apply
 	git checkout stash -- . # force git stash to overwrite added files
 else
