47a58e3a02c6f6eebd6d98ff752c66901b8c876f
François Moreews <fjrmoreews@gmail.com>
2020-06-25 10:05:22 +0200

    refactor+improve

--- pages-builder.sh
+++ pages-builder.sh
@@ -1,68 +1,90 @@
 #!/bin/bash
-# http://redsymbol.net/articles/unofficial-bash-strict-mode/
+
 set -euo pipefail
 IFS=$'\n\t'
 
-buildDirectory=_build
-SOURCEDIR=/home/fmoreews/git/pax2graphml_src/docs
+
 # get a clean master branch assuming
-git checkout master
-git pull origin master
-git clean -df
-git checkout -- .
-git fetch --all
+function getcleammaster {
+  echo "getcleammaster"
+  git checkout master
+  git pull origin master
+  git clean -df
+  git checkout -- .
+  git fetch --all
+}
 
-echo "step 1"
-# build html docs from sphinx files
-CDIR=$PWD
+function docbuild {
+  echo "docbuild"
+  SOURCEDIR=/home/fmoreews/git/pax2graphml_src/docs
+  # build html docs from sphinx files
+  CDIR=$PWD
 
-cd $SOURCEDIR/.. && bash build-doc.sh 
-#sphinx-build -b html . "$buildDirectory"
-cd $CDIR
-cp -r  $SOURCEDIR/* ./ 
-dat=`date "+%D" `
-echo "" > build.log
-sed -ri "1s#(.*)#\#auto-build ${dat}\\n\1#" build.log 
+  cd $SOURCEDIR/.. && bash build-doc.sh 
+  #sphinx-build -b html . "_build"
+  cd $CDIR
+  cp -r  $SOURCEDIR/* ./ 
+  dat=`date "+%D" `
+  echo "" > build.log
+  sed -ri "1s#(.*)#\#auto-build ${dat}\\n\1#" build.log 
+}
 
+function initghpages {
 
+  echo "initghpages"
+  # create or use orphaned gh-pages branch
+  BRANCH=gh-pages
+  if [ $(git branch --list "$BRANCH") ]
+  then
 
-# create or use orphaned gh-pages branch
-branch_name=gh-pages
-if [ $(git branch --list "$branch_name") ]
-then
-
-        echo "step 2"
-
+        echo "$BRANCH exists"
 	git stash
-	git checkout $branch_name
-	git pull origin $branch_name --allow-unrelated-histories
-	#git stash apply
+	git checkout $BRANCH
+	git pull origin $BRANCH --allow-unrelated-histories
 	git checkout stash -- . || echo "warning:$?" 
-        echo "step 2.2"
-else
-        echo "step 3"
+         
+  else
+        echo "GOING tO CREATE $BRANCH"
 
-	git checkout --orphan "$branch_name"
-fi
+	git checkout --orphan "$BRANCH"
+  fi
+}
 
-if [ -d "$buildDirectory" ]
-then
+function listexport {
+  echo "list export"
+  ls | grep -v _build
+}
 
-        echo "step 4"
+function adddoc {
+        echo "adddoc"
         ls | grep -v _build | xargs rm -r
 	mv _build/* . && rm -rf _build
 	git add .
-        echo "step 4.2" 
-
+         
 	git commit -m "new pages version $(date)" || echo "warning:$?"
 	git push origin gh-pages || echo "warning:$?"
-        echo "step 5"
 
-	# github.com recognizes gh-pages branch and create pages
-	# url scheme https//:[github-handle].github.io/[repository]
+}
+
+
+echo "start"
+getcleammaster
+initghpages
+docbuild
+if [ -d "_build" ]
+then
+  listexport
+  #adddoc
 else
-	echo "directory $buildDirectory does not exists"
+	echo "directory _build does not exists"
 fi
-echo "step 6"
+echo "checkout master"
 git checkout master
+echo "end"
+
+
+
+
+
+
 
